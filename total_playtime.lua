30d79b561aa69d73dc4da2c98b1c177d7093f27d
oltodosel <oltodosel@users.noreply.github.com>
2018-03-14 05:02:16 +0200

    Shows total playtime of current playlist.
    Added saving probed files for future reference.

--- total_playtime.lua
+++ total_playtime.lua
@@ -2,7 +2,14 @@
 --~ If number of items in playlist didn't change since last calculation - it doesn't probe files anew.
 --~ requires ffprobe (ffmpeg)
 
---~ F12 key_binding
+key_binding = 'F12'
+-- save probed files to a file for future reference -- ${fname} \t ${duration}
+save_probed = true
+saved_probed_filename = '~/.config/mpv/scripts/total_playtime.list'
+
+-----------------------------------
+
+saved_probed_filename = saved_probed_filename:gsub('~', os.getenv('HOME'))
 
 local utils = require 'mp.utils'
 
@@ -14,11 +21,27 @@ function disp_time(time)
 	return string.format("%02d:%02d:%02d", hours, minutes, seconds)
 end
 
+function setContains(set, key)
+	return set[key] ~= nil
+end
 playlist = {}
 playlist_total = -1
 
-function total_time()	
+function total_time()
 	if #playlist ~= playlist_total then
+		if save_probed then
+			if io.open(saved_probed_filename, "rb") then
+				probed_file = {}
+				for line in io.lines(saved_probed_filename) do
+					for k, v in line:gmatch("(.+)\t(.+)") do
+						probed_file[k] = v
+					end
+				end
+			else
+				probed_file = {}
+			end
+		end
+		
 		local cwd = utils.getcwd()
 		for _, f in ipairs(mp.get_property_native("playlist")) do
 			f = utils.join_path(cwd, f.filename)
@@ -32,8 +55,21 @@ function total_time()
 				f, n = string.gsub(f, "/[^/]*/%.%./", "/", 1)
 			until n == 0
 			
-			f = string.gsub(f, "\"", "\\\"") 
-			playlist[#playlist + 1] = { f, tonumber( io.popen('ffprobe -v quiet -of csv=p=0 -show_entries format=duration "'.. f .. '"'):read() ) }
+			f = string.gsub(f, "\"", "\\\"")
+			
+			if probed_file[f] then
+				fprobe = probed_file[f]
+			else
+				fprobe = io.popen('ffprobe -v quiet -of csv=p=0 -show_entries format=duration "'.. f .. '"'):read()
+				
+				if save_probed then
+					file = io.open(saved_probed_filename, "a")
+					file:write(f .. '\t' .. fprobe .."\n")
+					file:close()
+				end
+			end
+			
+			playlist[#playlist + 1] = { f, tonumber(fprobe) }
 			
 			mp.osd_message(string.format("Calculating: %s/%s", #playlist, mp.get_property("playlist-count")))
 		end
@@ -54,4 +90,4 @@ function total_time()
 	mp.osd_message(string.format("%s/%s \n %s/%s", disp_time(played_dur), disp_time(total_dur), mp.get_property("playlist-pos-1"), mp.get_property("playlist-count")))
 end
 
-mp.add_forced_key_binding("F12", "total_time", total_time)
+mp.add_forced_key_binding(key_binding, "total_time", total_time)
