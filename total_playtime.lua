880f5cc8fdc577ac6e58836e31f91e1be2d39f10
oltodosel <oltodosel@users.noreply.github.com>
2019-08-07 10:01:39 +0300

    Update total_playtime.lua

--- total_playtime.lua
+++ total_playtime.lua
@@ -43,7 +43,7 @@ function total_time()
 		end
 		
 		local cwd = utils.getcwd()
-		for _, f in ipairs(mp.get_property_native("playlist")) do
+		for pl_num, f in ipairs(mp.get_property_native("playlist")) do
 			f = utils.join_path(cwd, f.filename)
 			-- attempt basic path normalization
 			if on_windows then
@@ -62,14 +62,14 @@ function total_time()
 			else
 				fprobe = io.popen('ffprobe -v quiet -of csv=p=0 -show_entries format=duration "'.. f .. '"'):read()
 				
-				if save_probed then
+				if fprobe and save_probed then
 					file = io.open(saved_probed_filename, "a")
 					file:write(f .. '\t' .. fprobe .."\n")
 					file:close()
 				end
 			end
 			
-			playlist[#playlist + 1] = { f, tonumber(fprobe) }
+			playlist[#playlist + 1] = { f, tonumber(fprobe), pl_num }
 			
 			mp.osd_message(string.format("Calculating: %s/%s", #playlist, mp.get_property("playlist-count")))
 		end
@@ -93,3 +93,59 @@ function total_time()
 end
 
 mp.add_forced_key_binding(key_binding, "total_time", total_time)
+
+--------------------------------------------------
+--------------------------------------------------
+--------------------------------------------------
+
+function sort_playlist_to_0()
+	sort_playlist(1)
+end
+
+reverse = 0
+function sort_playlist(start_0)
+	total_time()
+
+	table.sort(playlist, function (left, right)
+		if reverse == 0 then
+			return left[2] < right[2]
+		else
+			return left[2] > right[2]
+		end
+	end)
+
+	if reverse == 0 then
+		reverse = 1
+	else
+		reverse = 0
+	end
+
+	out = ''
+
+	for i, f in pairs(playlist) do
+		if f[2] ~= nil then			
+			for i2, f2 in ipairs(mp.get_property_native("playlist")) do
+				if f2.filename == f[1] then
+					mp.commandv('playlist-move', i2 - 1, i - 1)
+					
+					if f2.filename == mp.get_property("path") then
+						-- out = string.format('%s>>%s\t\t%s\n', out, disp_time(f[2]), f[1]:gsub('.+/', ''))
+						out = string.format('%s%s\t\t%s\n', out, disp_time(f[2]), f[1]:gsub('.+/', ''))
+					else
+						out = string.format('%s%s\t\t%s\n', out, disp_time(f[2]), f[1]:gsub('.+/', ''))
+					end
+					break
+				end
+			end
+		end
+	end
+
+	if start_0 ~= nil then
+		mp.set_property('playlist-pos', 0)
+		os.execute('sleep .1')
+	end
+	mp.osd_message(out, 3)
+end
+
+mp.add_forced_key_binding('KP4', "sort_playlist", sort_playlist)
+mp.add_forced_key_binding('shift+KP4', "sort_playlist_to_0", sort_playlist_to_0)
